00ac0d83501a154d6c18df451ec4013b643d15f8
Jared Wilkening <jared.wilkening@gmail.com>
2012-08-16 11:47:38 -0700

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh 
 
 SERVICE_DIR=$1
+PERL_LIB="/kb/runtime/lib/perl5/site_perl/5.16.0"
 
 # build shock
 echo "installing shock"
@@ -29,6 +30,10 @@ echo "installing uploader"
 cp -r uploader ${SERVICE_DIR}
 ./setup.pl -input conf/uploader.cfg -output ${SERVICE_DIR}/uploader/UploaderConfig.pm
 cp conf/uploader.apache.conf /etc/apache2/sites
+
+/kb/runtime/bin/perl Bio-KBase-Auth/Build.PL 
+/kb/runtime/bin/perl Bio-KBase-Auth/Build installdeps --install_path arch=${PERL_LIB}
+/kb/runtime/bin/perl Bio-KBase-Auth/Build install --install_path arch=${PERL_LIB}
 
 # services
 cp services/* ${SERVICE_DIR}
