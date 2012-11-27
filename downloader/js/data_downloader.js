b381817463a8393821abbd6a9ccd0ddb89d1fa61
Paul Frybarger <pfrybarger@gmail.com>
2012-11-27 22:10:58 +0000

    Changes to data_downloader

--- downloader/js/data_downloader.js
+++ downloader/js/data_downloader.js
@@ -1,67 +1,94 @@
-// main routing file for data_downloader
+(function($) {
+    var type_cache = {};
 
-$(document).ready(function() {
-    // grab the available types
-    var typesAjax = $.ajax({
-        type: 'GET',
-        url:  'types.json'
-    })
+    // define the routes
+    var app = $.sammy(function() {
 
-    var countAjax = $.ajax({
-        type: 'GET',
-        url:  'type_counts.json',
-    })
+        this.get('#/', function() {
+            $('#main').text('');
+        });
+        
+        this.get('#/test', function() {
+            $('#main').text('Hello World');
+        });
+        
+    });
 
-    $.when(typesAjax, countAjax).done(function(types, counts){
-        addTypes(types[0], counts[0]);
-    }).fail(ajaxError);
+    $(document).ready(function() {
+        // grab the available types
+            var typesAjax = $.ajax({
+                type: 'GET',
+                url:  'types.json'
+            });
 
-});
+        var countAjax = $.ajax({
+            type: 'GET',
+            url:  'type_counts.json'
+        });
 
-function addTypes(types, counts) {
-    for (var i in types) {
-        var type = types[i];
-        $('#type-header').append('<div class="type-widget"> \
-                                    <div class="well well-small">'+type+' \
-                                    <span class="badge badge-inverse">'+counts[i]+'<br> \
-                                    </div> \
-                                  </div>');
+        $.when(typesAjax, countAjax).done(function(types, counts){
+            addTypeRoutes(types[0]);
+            addTypes(types[0], counts[0]);
+        }).fail(ajaxError);
 
+    });
+
+    function addTypeRoutes(types) {
+    
     }
+
+    function addTypes(types, counts) {
+        for (var i in types) {
+            var type = types[i];
+            $('#type-header').append('<div class="type-widget"> \
+                                        <div class="well well-small">'+type+' \
+                                          <span class="badge badge-inverse">'+counts[i]+'<br> \
+                                        </div> \
+                                      </div>');
+
+        }
 
 //<span class="badge badge-inverse">
 
-}
-
-function getNodesForType(type) {
-    $.ajax({
-        type: 'GET',
-        url:  'http://kbase.us/services/shock-api/node?query&type=' + type
-    }).done(loadDataTable)
-      .fail(ajaxError);
-}
-
-function loadDataTable(ajaxData) {
-    // check for error
-    if (ajaxData.E !== null) {
-        alert('error loading data');
-        console.log(ajaxData.E, ajaxData.S);
-        return;
     }
 
-    var data = ajaxData.D;
-    console.log(data);
-}
+    function getNodesForType(type) {
+        if (type_cache[type]) {
+            console.log('loading type data from cache: ' + type);
+            loadDataTable(type_cache[type]);
+        } else {
+            $('#type-table').empty().append('loading...');
+            console.log('loading type data from AJAX call: ' + type);
+            $.ajax({
+                type: 'GET',
+                url:  'http://kbase.us/services/shock-api/node?query&type=' + type
+            }).done(function(data) {
+                // check for error
+                if (data.E !== null) {
+                    alert('error loading data');
+                    console.log(data.E, data.S);
+                } else {
+                    type_cache[type] = data.D;
+                    loadDataTable(data.D);
+                }
+            }).fail(ajaxError);
+        }
+    }
 
-function ajaxError(jqXHR, textStatus, errorThrown) {
-    alert('error with ajax call');
-    console.log(jqXHR, textStatus, errorThrown);
-}
+    function loadDataTable(data) {
+        $('#type-table').empty().append('data');
+        console.log(data);
+    }
 
+    function ajaxError(jqXHR, textStatus, errorThrown) {
+        alert('error with ajax call');
+        console.log(jqXHR, textStatus, errorThrown);
+    }
 
-function addTestTable() {
-    $('body').append('<table id="table"><thead><tr><th>Column 1</th><th>Column 2</th><th>etc</th>' +
-		     '</tr></thead><tbody><tr><td>Row 1 Data 1</td><td>Row 1 Data 2</td><td>etc</td></tr><tr>' +
-		     '<td>Row 2 Data 1</td><td>Row 2 Data 2</td><td>etc</td></tr></tbody></table>');
-    $('#table').dataTable();
-}
+    function addTestTable() {
+        $('body').append('<table id="table"><thead><tr><th>Column 1</th><th>Column 2</th><th>etc</th>' +
+		         '</tr></thead><tbody><tr><td>Row 1 Data 1</td><td>Row 1 Data 2</td><td>etc</td></tr><tr>' +
+		         '<td>Row 2 Data 1</td><td>Row 2 Data 2</td><td>etc</td></tr></tbody></table>');
+        $('#table').dataTable();
+    }
+})(jQuery);
