27e389f7112af2806bcb08453396f2729c7b62d7
Paul Frybarger <pfrybarger@gmail.com>
2012-11-27 21:07:02 +0000

    data_downloader changes

--- downloader/js/data_downloader.js
+++ downloader/js/data_downloader.js
@@ -5,33 +5,39 @@ $(document).ready(function() {
     $.ajax({
         type: 'GET',
         url:  'types.json'
-    }).done(function(types) {
-        addTypes(types);
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-        alert('error with ajax call');
-        console.log(jqXHR, textStatus, errorThrown);
-    });
-
-    var button = $('<button>Add Table</button>');
-    button.click(function() {
-        $.ajax({
-            type: 'GET',
-            url:  'http://kbase.us/services/shock-api/node'
-        }).done(function(data) {
-            console.log(data);
-        }).fail(function(jqXHR, textStatus, errorThrown) {
-            alert('error with ajax call');
-            console.log(jqXHR, textStatus, errorThrown);
-        });
-//	addTestTable();
-	button.get(0).disabled = 'disabled';
-    });
-    $('body').append(button);
+    }).done(addTypes)
+      .fail(ajaxError);
 });
 
 function addTypes(types) {
-    
+
 }
+
+function getNodesForType(type) {
+    $.ajax({
+        type: 'GET',
+        url:  'http://kbase.us/services/shock-api/node?query&type=' + type
+    }).done(loadDataTable)
+      .fail(ajaxError);
+}
+
+function loadDataTable(ajaxData) {
+    // check for error
+    if (ajaxData.E !== null) {
+        alert('error loading data');
+        console.log(ajaxData.E, ajaxData.S);
+        return;
+    }
+
+    var data = ajaxData.D;
+    console.log(data);
+}
+
+function ajaxError(jqXHR, textStatus, errorThrown) {
+    alert('error with ajax call');
+    console.log(jqXHR, textStatus, errorThrown);
+}
+
 
 function addTestTable() {
     $('body').append('<table id="table"><thead><tr><th>Column 1</th><th>Column 2</th><th>etc</th>' +
