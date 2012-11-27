34bc574d480e8b83bd45f4ceae7733d0be79cb45
Neal Conrad <nealconrad@gmail.com>
2012-11-27 16:49:24 -0600

    update

--- downloader/js/data_downloader.js
+++ downloader/js/data_downloader.js
@@ -30,16 +30,18 @@
             app.run();
         }).fail(ajaxError);
 
+        addTestTable()
+
     });
 
     function addTypes(types, counts) {
         for (var i in types) {
             var type = types[i];
-            $('#type-header').append('<a href="'+type+'" ><div class="type-widget"> \
+            $('#type-header').append('<div class="type-widget"><a href="#'+type+'" > \
                                         <div class="well well-small">'+type+' \
                                         <span class="badge badge-inverse">'+counts[i]+'<br> \
-                                        </div> \
-                                      </div></a>');
+                                        </div></a> \
+                                      </div>');
         }
     }
 
@@ -84,7 +86,7 @@
     }
 
     function addTestTable() {
-        $('body').append('<table id="table"><thead><tr><th>Column 1</th><th>Column 2</th><th>etc</th>' +
+        $('#type-table').append('<table id="table"><thead><tr><th>Column 1</th><th>Column 2</th><th>etc</th>' +
 		         '</tr></thead><tbody><tr><td>Row 1 Data 1</td><td>Row 1 Data 2</td><td>etc</td></tr><tr>' +
 		         '<td>Row 2 Data 1</td><td>Row 2 Data 2</td><td>etc</td></tr></tbody></table>');
         $('#table').dataTable();
