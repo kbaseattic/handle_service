722f377dc8b8bd3adc950ec682cbf5078db78542
brettin <brettin@anl.gov>
2014-05-27 16:32:09 +0000

    removed back tics as those are not welcome in the mysql shell

--- hsi.sql
+++ hsi.sql
@@ -1,15 +1,15 @@
-CREATE DATABASE `hsi`;
+CREATE DATABASE hsi;
 USE hsi;
-CREATE TABLE IF NOT EXISTS `Handle` (
-	`id`	  	varchar(256) NOT NULL DEFAULT '',
-	`file_name`     varchar(256),
-	`type` 		varchar(256),
-	`url` 		varchar(256),
-	`remote_md5` 	varchar(256),
-	`remote_sha1`	varchar(256),
-	`created_by`	varchar(256),
-	`creation_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS Handle (
+	id	  	varchar(256) NOT NULL DEFAULT '',
+	file_name     varchar(256),
+	type 		varchar(256),
+	url 		varchar(256),
+	remote_md5 	varchar(256),
+	remote_sha1	varchar(256),
+	created_by	varchar(256),
+	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
 ); 
 GRANT SELECT,INSERT,UPDATE,DELETE 
 	ON hsi.*
